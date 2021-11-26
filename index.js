17f65924f5d03086a884ec2dc2c196d70ad8e71d
yeasin Rahman <yeasinrahman96@gmail.com>
2021-11-26 11:05:30 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const btn1=document.getElementById('btn1')
+        const btn2=document.getElementById('btn2')
+        btn1.addEventListener('click',()=>{
+document.getElementById("img1").src='/light-bulb-png-file.png'
+        })
+        btn2.addEventListener('click',()=>{
+document.getElementById("img1").src='/6-2-light-bulb-free-download-png.png'
+        })
